7b3f177c7ac3379553b7c826f7b55d5998ae43fa
Rameel Sethi <29188824+rameelua@users.noreply.github.com>
2018-08-24 15:57:06 -0600

    Add files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PlayerTemplate from './PlayerTemplate.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PlayerTemplate />
+      </div>
+    );
+  }
+}
+
+export default App;
